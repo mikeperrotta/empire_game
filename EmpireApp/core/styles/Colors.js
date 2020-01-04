b38dd4e1721a7a655193fe29021308b9b997fdd6
Mike <perrotta@neosensory.com>
2020-01-04 14:32:34 -0800

    Setup home page buttons and logo

---
+++ EmpireApp/core/styles/Colors.js
@@ -0,0 +1,5 @@
+export const Colors = {
+  BOLD_BLUE: '#2699FB',
+  DARK_BLUE: '#007FEB',
+  WHITE_TEXT: '#FFFFFF',
+};
--- EmpireApp/pages/Home.js
+++ EmpireApp/pages/Home.js
@@ -1,7 +1,41 @@
 // Home.js
 
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, Image, ImageBackground, Button, TouchableHighlight } from 'react-native';
+
+import { Colors } from '../core/styles/Colors';
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    height: '100%',
+    width: '100%',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: Colors.BOLD_BLUE,
+    borderRadius: 5,
+    height: 48,
+    padding: 10,
+    width: 148,
+    margin: 20,
+  },
+  buttonContainer: {
+    margin: 30,
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'space-between',
+    margin: 60,
+  },
+  logoImage: {
+  },
+  text: {
+    color: Colors.WHITE_TEXT,
+    fontFamily: 'HelveticaNeue',
+    fontSize: 20,
+  },
+});
 
 export class Home extends Component {
   navigate = (toScreen) => {
@@ -12,12 +46,29 @@ export class Home extends Component {
   render() {
     return (
       <ImageBackground
-        style={{width: '100%', height: '100%'}}
-        source={require('../assets/homepageBackground.png')}
-      >
-        <Text>
-          Text!
-        </Text>
+          style={styles.backgroundImage}
+          source={require('../assets/homepageBackground.png')}>
+        <View style={styles.container}>
+          <Image
+              style={styles.logoImage}
+              source={require('../assets/logo.png')} />
+          <View style={styles.buttonContainer}>
+            <TouchableHighlight
+                activeOpacity={1}
+                onPress={() => this.navigate('HomeScreen')}
+                style={styles.button}
+                underlayColor={Colors.DARK_BLUE}>
+              <Text style={styles.text}> Play </Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+                activeOpacity={1}
+                onPress={() => this.navigate('HomeScreen')}
+                style={styles.button}
+                underlayColor={Colors.DARK_BLUE}>
+              <Text style={styles.text}> Rules </Text>
+            </TouchableHighlight>
+          </View>
+        </View>
       </ImageBackground>
     );
   }
