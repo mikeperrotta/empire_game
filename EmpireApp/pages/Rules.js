50ca073bfa175beefd89ebb975bfa564835e09b6
Mike <perrotta@neosensory.com>
2020-01-04 15:32:54 -0800

    Add rules in scrollview

--- EmpireApp/pages/Rules.js
+++ EmpireApp/pages/Rules.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableHighlight, Image } from 'react-native';
+import { StyleSheet, ScrollView, View, Text, TouchableHighlight, Image } from 'react-native';
 
 import { Colors } from '../core/styles/Colors';
 
@@ -9,10 +9,26 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerContainer: {
-    marginTop: 34,
-    marginLeft: 16,
-  }
+    marginTop: 40,
+    marginLeft: 26,
+  },
+  titleText: {
+    marginVertical: 30,
+    marginHorizontal: 32,
+    fontFamily: 'Georgia',
+    color: Colors.BOLD_BLUE,
+    fontSize: 32,
+  },
+  rulesText: {
+    fontSize: 14,
+    fontFamily: 'HelveticaNeue',
+    color: Colors.BOLD_BLUE,
+    margin: 32,
+    lineHeight: 24,
+  },
 });
+
+const rulesText = "In Empire, players will answer a group question and will have to guess which player provided which answer. \n\nTo begin the game, press “Play” and pick one of the questions from the list.\n\nThen, each player thinks of her personal answer for that question. When a player is ready, she takes the phone and types in her answer.\n\nIf two players provide the same answer (or very similar answers), both of their answers will be removed and they must both enter in new answers.\n\nThe game begins once each player has entered her answer. At the start of the game, players read the answer list, which shows all of the answers plus a few fake answers that players did not choose.\n\nDecide which player will go first. That player attempts to match one of the answers with another player. For instance, if it is Helen’s turn, she could say “Alex, I think your answer is Cleopatra”. \n\nIf Helen was wrong, then Alex goes next and guesses any player. \n\nIf Helen was right, Alex joins her empire and Helen gets to guess again. \n\nAs empress, Helen can consult with Alex but Helen gets the final decision in guessing.\n\nIf another player correctly guesses Helen, Helen and her entire empire become part of that other player’s empire. \n\nThe winning player is whoever becomes emperor of all other players. \n";
 
 export class Rules extends Component {
   navigate = (toScreen) => {
@@ -26,11 +42,18 @@ export class Rules extends Component {
         <View style={styles.headerContainer}>
           <TouchableHighlight
               onPress={() => this.navigate('HomeScreen')}
-              style={styles.button}
               underlayColor={Colors.WHITE}>
             <Image source={require('../assets/backArrow.png')} />
           </TouchableHighlight>
         </View>
+        <Text style={styles.titleText}>
+          Rules
+        </Text>
+        <ScrollView>
+          <Text style={styles.rulesText}>
+            {rulesText}
+          </Text>
+        </ScrollView>
       </View>
     );
   }
