5f2d32a223547d9d1aea740a31faae51bb361a10
Mike <perrotta@neosensory.com>
2020-01-04 14:55:08 -0800

    Make back button on rules page

--- EmpireApp/core/styles/Colors.js
+++ EmpireApp/core/styles/Colors.js
@@ -1,5 +1,6 @@
 export const Colors = {
   BOLD_BLUE: '#2699FB',
   DARK_BLUE: '#007FEB',
+  WHITE: '#FFFFFF',
   WHITE_TEXT: '#FFFFFF',
 };
--- EmpireApp/pages/Rules.js
+++ EmpireApp/pages/Rules.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight, Image } from 'react-native';
 
 import { Colors } from '../core/styles/Colors';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    flex: 1,
+  },
+  headerContainer: {
+    marginTop: 34,
+    marginLeft: 16,
+  }
+});
 
 export class Rules extends Component {
+  navigate = (toScreen) => {
+    const { navigation } = this.props;
+    navigation.navigate(toScreen)
+  }
+
   render() {
     return (
-      <Text> text </Text>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <TouchableHighlight
+              onPress={() => this.navigate('HomeScreen')}
+              style={styles.button}
+              underlayColor={Colors.WHITE}>
+            <Image source={require('../assets/backArrow.png')} />
+          </TouchableHighlight>
+        </View>
+      </View>
     );
   }
 }
