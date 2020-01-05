62d9ccc211e35a8f9545b324627eb4ac052f9ecd
Mike <perrotta@neosensory.com>
2020-01-04 17:25:46 -0800

    Create Questions.js

---
+++ EmpireApp/assets/Questions.js
@@ -0,0 +1,66 @@
+const QUESTIONS = [
+  {
+  	question: 'If you were a historical figure, who would you be?',
+  	answers: [
+  		'Cleopatra',
+  		'Ada Lovelace',
+  		'Homer',
+  		'Malcolm X',
+  		'Marco Polo',
+  		'Galileo Galilei',
+  		'Frida Kahlo',
+  		'J. R. R. Tolkien',
+  		'Emily Dickinson',
+  		'Roald Dahl',
+  		'Simon Bolivar',
+  		'Elvis Presley',
+  		'Che Guevara',
+  		'Barack Obama',
+  		'Jacques Cousteau',
+  		'Queen Elizabeth I',
+  		'Nikola Tesla',
+  		'George Washington Carver',
+    ]
+  },
+  {
+  	question: 'What era would you time travel to?',
+  	answers: [
+  		'Jurassic Period',
+  		'Middle Ages',
+  		'Industrial Revolution',
+  		'World War II',
+  		'Ancient Greece',
+  		'Stone Age',
+  		'Renaissance',
+  		'Ancient Rome',
+    ]
+  },
+  {
+  	question: 'What animal best represents you?',
+  	answers: [
+  		'Bear',
+  		'Camel',
+  		'Cat',
+  		'Cheetah',
+  		'Eagle',
+  		'Horse',
+  		'Monkey',
+  		'Rabbit',
+  		'Turtle',
+    ]
+  },
+  {
+  	question: 'What food would you choose to eat for the rest of your life?',
+  	answers: [
+  		'Salad',
+  		'Grilled cheese',
+  		'Pizza',
+  		'Steak',
+  		'Sushi',
+  		'Stir fry',
+  		'PB&J',
+    ]
+  },
+];
+
+export default QUESTIONS;
