2a888c69ac4d55a969b5ae4888ec790419436ad0
Mike <perrotta@neosensory.com>
2020-01-09 00:40:41 -0800

    Add more questions

--- EmpireApp/assets/Questions.js
+++ EmpireApp/assets/Questions.js
@@ -1,66 +1,138 @@
-const QUESTIONS = [
-  {
-  	question: 'If you were a historical figure, who would you be?',
-  	answers: [
-  		'Cleopatra',
-  		'Ada Lovelace',
-  		'Homer',
-  		'Malcolm X',
-  		'Marco Polo',
-  		'Galileo Galilei',
-  		'Frida Kahlo',
-  		'J. R. R. Tolkien',
-  		'Emily Dickinson',
-  		'Roald Dahl',
-  		'Simon Bolivar',
-  		'Elvis Presley',
-  		'Che Guevara',
-  		'Barack Obama',
-  		'Jacques Cousteau',
-  		'Queen Elizabeth I',
-  		'Nikola Tesla',
-  		'George Washington Carver',
-    ]
-  },
-  {
-  	question: 'What era would you time travel to?',
-  	answers: [
-  		'Jurassic Period',
-  		'Middle Ages',
-  		'Industrial Revolution',
-  		'World War II',
-  		'Ancient Greece',
-  		'Stone Age',
-  		'Renaissance',
-  		'Ancient Rome',
-    ]
-  },
-  {
-  	question: 'What animal best represents you?',
-  	answers: [
-  		'Bear',
-  		'Camel',
-  		'Cat',
-  		'Cheetah',
-  		'Eagle',
-  		'Horse',
-  		'Monkey',
-  		'Rabbit',
-  		'Turtle',
-    ]
-  },
-  {
-  	question: 'What food would you choose to eat for the rest of your life?',
-  	answers: [
-  		'Salad',
-  		'Grilled cheese',
-  		'Pizza',
-  		'Steak',
-  		'Sushi',
-  		'Stir fry',
-  		'PB&J',
-    ]
-  },
-];
+const QUESTIONS = [{
+  question: 'If you were a historical figure, who would you be?',
+  answers: [
+    'Cleopatra',
+    'Ada Lovelace',
+    'Homer',
+    'Malcolm X',
+    'Marco Polo',
+    'Galileo Galilei',
+    'Frida Kahlo',
+    'J. R. R. Tolkien',
+    'Emily Dickinson',
+    'Roald Dahl',
+    'Simon Bolivar',
+    'Elvis Presley',
+    'Che Guevara',
+    'Barack Obama',
+    'Jacques Cousteau',
+    'Queen Elizabeth I',
+    'Nikola Tesla',
+    'George Washington Carver',
+  ]
+}, {
+  question: 'What era would you time travel to?',
+  answers: [
+    'Jurassic Period',
+    'Middle Ages',
+    'Industrial Revolution',
+    'World War II',
+    'Ancient Greece',
+    'Stone Age',
+    'Renaissance',
+    'Ancient Rome',
+  ]
+}, {
+  question: 'What animal best represents you?',
+  answers: [
+    'Bear',
+    'Camel',
+    'Cat',
+    'Cheetah',
+    'Eagle',
+    'Horse',
+    'Monkey',
+    'Rabbit',
+    'Turtle',
+  ]
+}, {
+  question: 'What food would you choose to eat for the rest of your life?',
+  answers: [
+    'Salad',
+    'Grilled cheese',
+    'Pizza',
+    'Steak',
+    'Sushi',
+    'Stir fry',
+    'PB&J',
+  ]
+}, {
+  question: 'Who would you get lunch with, living or dead?',
+  answers: [
+    'Cleopatra',
+    'Ada Lovelace',
+    'Homer',
+    'Malcolm X',
+    'Marco Polo',
+    'Galileo Galilei',
+    'Frida Kahlo',
+    'J. R. R. Tolkien',
+    'Emily Dickinson',
+    'Jesus Christ',
+  ]
+}, {
+  question: 'What\'s the weirdest thing you\'ve eaten?',
+  answers: [
+    'Snails',
+    'Chicken feet',
+    'Haggis',
+    'Blood Sausage',
+    'Spam',
+  ]
+}, {
+  question: 'What\'s a guilty pleasure of yours?',
+  answers: [
+    'Third Eye Blind',
+    'Sleeping naked',
+    'Geneology tracing',
+    'Tabloids',
+    'Peanut butter from the spoon',
+  ]
+}, {
+  question: 'What\'s the one thing you\'d take to a desert island?',
+  answers: [
+    'Blanket',
+    'Bottle of water',
+    'Rope',
+    'Volley ball',
+    'Boat',
+  ]
+}, {
+  question: 'What song should be played at your funeral?',
+  answers: [
+    'You Raise Me Up',
+    'Amazing Grace',
+    'Funeral Song',
+    'Hallelujah',
+    'Angel Band',
+  ]
+}, {
+  question: 'What fictional character would you want to be?',
+  answers: [
+    'Harry Potter',
+    'Sherlock Holmes',
+    'Gandalf',
+    'James Bond',
+    'Atticus Finch',
+  ]
+}, {
+  question: 'What\'s a hobby you want to pick up?',
+  answers: [
+    'Poker',
+    'Chess',
+    'Wine tasting',
+    'Piloting',
+    'Archery',
+  ]
+}, {
+  question: 'Where is your next dream vacation?',
+  answers: [
+    'Antarctica',
+    'Galapagos',
+    'Machu Picchu',
+    'Iceland',
+    'Santorini',
+  ]
+}];
 
 export default QUESTIONS;
